76029556aaf42c010d78a15c267b261ebdb8aa46
Mohamed.Salama <mohamedsalamaaa@gmail.com>
2023-03-04 00:43:48 +0100

    Finalizing the fetch with api

--- js/ui.js
+++ js/ui.js
@@ -1,34 +1,75 @@
-class ui {
+export class ui {
   constructor() {
-    this.getGamesData();
+    this.getGamesData("MMORPG");
   }
 
-  async getGamesData() {
-    async function fetchData() {
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "c4c10201e6msh32b0f012d5f24b6p182113jsn4b753d588fb1",
-          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
-        },
-      };
+  async getGamesData(category) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'c4c10201e6msh32b0f012d5f24b6p182113jsn4b753d588fb1',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+      }
+    };
 
-      const response = await fetch(
-        "https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter",
-        options
-      );
+    const response = await fetch(
+      `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`,
+      options
+    );
 
-      const jsonData = await response.json();
+    const jsonData = await response.json();
 
-      return jsonData;
+
+    this.displayData(jsonData, category);
+  }
+
+  displayData(data, category) {
+    let rows = document.querySelector("#cards-row");
+
+
+    let container = ``;
+
+    for (const game of data) {
+      container += `<div class="col-lg-3 col-md-6 col-sm-12 mt-3" >
+      <div>
+        <div class="card bg-transparent p-3 style="width: 18rem">
+        <div class="cards">
+          <img
+            src=${game.thumbnail}
+            class="card-img-top w-100"
+            alt="..."
+          />
+
+          <div class="card-body">
+            <span class="d-flex justify-content-between">
+              <h5 class="card-title text-white fs-6">
+              ${game.title}
+              </h5>
+              <p class="bg-secondary d-inline px-2 py-1 rounded-2">
+                Free
+              </p>
+            </span>
+
+            <p class="card-text text-white border-bottom pb-3 game-info text-center " style="font-size: 14px;height: 120px ;" >
+              S${game.short_description}
+            </p>
+
+            <div class="d-flex justify-content-between">
+              <p class="text-white bg-gradient px-2 py-1 cards-fs m-0 game-category">
+                ${category}
+              </p>
+
+              <p class="text-white bg-gradient px-2 py-1 cards-fs m-0 game-console">
+                ${game.platform}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
     }
 
-    const data = await fetchData();
-    console.log(data); // logs the JSON data to the console
-    // do something with the JSON data here
-    return data;
+    rows.innerHTML = container;
   }
 }
-
-let games = new ui();

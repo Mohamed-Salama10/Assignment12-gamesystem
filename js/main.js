da49a07b0dd58128799c1470c54c816113144ce1
Mohamed.Salama <mohamedsalamaaa@gmail.com>
2023-03-04 01:47:10 +0100

    Implementing the code for the actions upon clicking on the nav bar icons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
-import { ui } from './ui.js';
+"use strict";
+
+import { ui } from "./ui.js";
 let game = new ui();
+let mmorpgButton = document.getElementById("mmorpgButton");
+let shooterButton = document.getElementById("shooterButton");
+let sailingButton = document.getElementById("sailingButton");
+let permadeathButton = document.getElementById("permadeathButton");
+let superheroButton = document.getElementById("superheroButton");
+let pixelButton = document.getElementById("pixelButton");
+
+let buttonsArray = [
+  mmorpgButton,
+  shooterButton,
+  sailingButton,
+  permadeathButton,
+  superheroButton,
+  pixelButton,
+];
+
+function removeActiveStatus(buttonsArray) {
+  for (const button of buttonsArray) {
+    console.log(button);
+    if (button.classList.contains("text-primary")) {
+      button.classList.replace("text-primary", "text-white");
+    }
+  }
+}
+
+
+mmorpgButton.addEventListener("click", function () {
+  changeDisplayedCategory(this);
+});
+shooterButton.addEventListener("click", function () {
+  changeDisplayedCategory(this);
+});
+sailingButton.addEventListener("click", function () {
+  changeDisplayedCategory(this);
+});
+
+shooterButton.addEventListener("click", function () {
+  changeDisplayedCategory(this);
+});
+
+permadeathButton.addEventListener("click", function () {
+    changeDisplayedCategory(this);
+  });
+  superheroButton.addEventListener("click", function () {
+    changeDisplayedCategory(this);
+  });
+  pixelButton.addEventListener("click", function () {
+    changeDisplayedCategory(this);
+  });
 
 
 
+
+  
+
+function changeDisplayedCategory(button) {
+  let category = button.innerText.toLowerCase();
+  game.getGamesData(category);
+  removeActiveStatus(buttonsArray);
+  button.classList.replace("text-white", "text-primary");
+}
